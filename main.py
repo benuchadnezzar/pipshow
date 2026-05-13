4b7afd853600fb931d7c5490308a550a0994e651
bkulajw <bkulakofsky@justworks.com>
2026-05-12 20:58:52 -0400

    Added one word to print statement at the end

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ with open(Path(__file__).parent / "packages.txt") as file:
     packages = [json.loads(line) for line in file if line.strip()]
 
 pkg = random.choice(packages)
-print(f"PipShow: Have you ever heard of {pkg['name']}? Let me tell you! {pkg['summary']}")
+print(f"PipShow: Have you ever heard of {pkg['name']}? Let me tell you! It's {pkg['summary']}")
