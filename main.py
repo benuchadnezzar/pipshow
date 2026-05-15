7cbed4d7a76c85ccb1eeff550f4eb2baa9c0da72
bkulajw <bkulakofsky@justworks.com>
2026-05-14 21:38:36 -0400

    Changed first letter of summary to lowercase

--- main.py
+++ main.py
@@ -6,4 +6,5 @@ with open(Path(__file__).parent / "packages.txt") as file:
     packages = [json.loads(line) for line in file if line.strip()]
 
 pkg = random.choice(packages)
+pkg["summary"] = pkg["summary"][0].lower() + pkg["summary"][1:]
 print(f"PipShow: Have you ever heard of {pkg['name']}? Let me tell you! It's {pkg['summary']}")
